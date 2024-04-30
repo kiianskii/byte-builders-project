301b4f2ec23d89c5f9b6c430ac986afa5fbd3881
kenyafan <kenyafan321@gmail.com>
2024-04-30 21:05:41 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
-import { Suspense } from 'react'
-import './App.css'
-import HomeTab from './pages/HomeTab/HomeTab'
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import "./App.css";
+import HomeTab from "./pages/HomeTab/HomeTab";
 
-
-import Layout from './components/Layout/Layout'
-import ErrorPage from './pages/ErrorPage/ErrorPage'
+import Layout from "./components/Layout/Layout";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 function App() {
-
-
   return (
     <Suspense fallback={null}>
-       <Routes>
-					<Route path='/' element={<Layout />}>
-					<Route index element={<HomeTab />} />
-					</Route>
-					<Route path='*' element={<ErrorPage />} />
-		</Routes>
-
-</Suspense>
-  )
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomeTab />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
