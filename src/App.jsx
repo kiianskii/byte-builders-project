25eee8c9253adbd82216b5419a7d76c09b06f510
Necrotic999 <l.ilya041105@gmail.com>
2024-05-01 17:52:04 +0300

    add switcher and base AddTransactionForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import { Suspense, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import HomeTab from "./pages/HomeTab/HomeTab";
 
-import { Route, Routes } from 'react-router-dom'
-import { Suspense, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import './App.css'
-import HomeTab from './pages/HomeTab/HomeTab'
-
-
-import Layout from './components/Layout/Layout'
-import ErrorPage from './pages/ErrorPage/ErrorPage'
+import Layout from "./components/Layout/Layout";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 // import { selectIsRefreshing } from './redux/auth/authSlice'
-import { refreshThunk } from './redux/auth/operations'
-
+import { refreshThunk } from "./redux/auth/operations";
+import ButtonAddTransactions from "./components/ButtonAddTransactions/ButtonAddTransactions";
 
 function App() {
+  const dispatch = useDispatch();
+  // const isRefreshing = useSelector(selectIsRefreshing)
 
-	const dispatch = useDispatch()
-	// const isRefreshing = useSelector(selectIsRefreshing)
-
-	useEffect(() => {
-		dispatch(refreshThunk())
-	}, [dispatch])
-
+  useEffect(() => {
+    dispatch(refreshThunk());
+  }, [dispatch]);
 
   return (
     <Suspense fallback={null}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomeTab />} />
+          <Route
+            path="/btnAddTransactions"
+            element={<ButtonAddTransactions />}
+          />
         </Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
