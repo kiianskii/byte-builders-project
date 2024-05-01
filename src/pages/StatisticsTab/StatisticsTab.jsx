bb35d6a277b7ac2a4f54f6e2effcb4a37058e9d4
AlexHrab <151546761+AlexHrab@users.noreply.github.com>
2024-05-01 19:18:26 +0300

    up

--- src/pages/StatisticsTab/StatisticsTab.jsx
+++ src/pages/StatisticsTab/StatisticsTab.jsx
@@ -13,48 +13,49 @@ function StatisticsTab() {
   }, [date]);
 
   return (
-    // <p>hello</p>
-    <div className={css.SearchBox}>
-      <select
-        name="month"
-        onChange={(e) => setDate({ ...date, month: +e.target.value })}
-        className={css.input}
-      >
-        <option value="1" selected>
-          January
-        </option>
-        <option value="2">February</option>
-        <option value="3">March</option>
-        <option value="4">April</option>
-        <option value="5">May</option>
-        <option value="6">June</option>
-        <option value="7">July</option>
-        <option value="8">August</option>
-        <option value="9">September</option>
-        <option value="10">October</option>
-        <option value="11">November</option>
-        <option value="12">December</option>
-      </select>
-      <select
-        name="year"
-        onChange={(e) => setDate({ ...date, year: +e.target.value })}
-        className={css.input}
-      >
-        <option value="2024" selected>
-          2024
-        </option>
-        <option value="2023">2023</option>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
-        <option value="2020">2020</option>
-        <option value="2019">2019</option>
-        <option value="2018">2018</option>
-        <option value="2017">2017</option>
-        <option value="2016">2016</option>
-        <option value="2015">2015</option>
-        <option value="2014">2014</option>
-        <option value="2013">2013</option>
-      </select>
+    <div className={css.box}>
+      <div className={css.SearchBox}>
+        <select
+          name="month"
+          onChange={(e) => setDate({ ...date, month: +e.target.value })}
+          className={css.input}
+        >
+          <option value="1" selected>
+            January
+          </option>
+          <option value="2">February</option>
+          <option value="3">March</option>
+          <option value="4">April</option>
+          <option value="5">May</option>
+          <option value="6">June</option>
+          <option value="7">July</option>
+          <option value="8">August</option>
+          <option value="9">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+        </select>
+        <select
+          name="year"
+          onChange={(e) => setDate({ ...date, year: +e.target.value })}
+          className={css.input}
+        >
+          <option value="2024" selected>
+            2024
+          </option>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+        </select>
+      </div>
     </div>
   );
 }
