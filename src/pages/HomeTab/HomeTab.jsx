5b1cdd591e2d438b07ac7ebb7ecb6d73b11e7447
kiianskii <ankiyanskiy@gmail.com>
2024-05-02 16:41:29 +0300

    add modal addform to hometab

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,23 +1,37 @@
-import TransactionList from "../../components/TransactionList/TransactionList"
-import { useEffect } from 'react'
-import { useDispatch } from "react-redux"
-import { transactionsCategoriesThunk, userTransactionsThunk } from "../../redux/transactions/operations";
-
-
+import TransactionList from "../../components/TransactionList/TransactionList";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {
+  transactionsCategoriesThunk,
+  userTransactionsThunk,
+} from "../../redux/transactions/operations";
+import { useToggle } from "../../hooks/useToggle";
+import css from "../../components/ButtonAddTransactions/ButtonAddTransactions.module.css";
+import AddTransactionForm from "../../components/AddTransactionForm/AddTransactionForm";
+import Modal from "../../components/Modal/Modal";
+import { Icon } from "../../img/Icon";
 
 function HomeTab() {
+  const dispatch = useDispatch();
+  const { openModal, closeModal, isOpen } = useToggle();
 
-  const dispatch = useDispatch()
-    
-      useEffect(() => {
-    dispatch(userTransactionsThunk())
-    dispatch(transactionsCategoriesThunk())
+  useEffect(() => {
+    dispatch(userTransactionsThunk());
+    dispatch(transactionsCategoriesThunk());
   }, [dispatch]);
   return (
     <div>
-      <TransactionList/>
+      <TransactionList />
+      <button className={css.add_btn} type="button" onClick={openModal}>
+        <Icon size={20} id="plus" />
+      </button>
+      {isOpen && (
+        <Modal title="Add Transaction" closeModal={closeModal}>
+          <AddTransactionForm closeModal={closeModal} />
+        </Modal>
+      )}
     </div>
-  )
+  );
 }
 
-export default HomeTab
+export default HomeTab;
