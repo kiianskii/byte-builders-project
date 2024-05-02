c1f0a79ddbdc0f245cb8dfab1ab0fec6b521cf61
Lulia131313 <146219985+Lulia131313@users.noreply.github.com>
2024-05-02 19:36:37 +0300

    get

--- src/components/TransactionItem/TransactionItem.jsx
+++ src/components/TransactionItem/TransactionItem.jsx
@@ -1,19 +1,40 @@
-import { deleteTransactionThunk } from '../../redux/transactions/operations'
-import s from './TransactionItem.module.css'
-import { useDispatch } from "react-redux"
+import { useToggle } from "../../hooks/useToggle";
+import { deleteTransactionThunk } from "../../redux/transactions/operations";
+import EditTransactionForm from "../AditTransactionForm/AditTransactionForm";
+import Modal from "../Modal/Modal";
+import s from "./TransactionItem.module.css";
+import { useDispatch } from "react-redux";
 
 function TransactionItem({ transaction }) {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  const { openModal, closeModal, isOpen } = useToggle();
+
   return (
-                <tr className={s.rowline}>
-                <td className={s.row}>{transaction.transactionDate}</td>
-                <td className={s.row}>{transaction.type === "INCOME"? "+":"-"}</td>
-                <td className={s.row}>{transaction.categoryId}</td>
-                <td className={s.row}>{transaction.comment}</td>
-                <td className={s.row}>{transaction.amount}</td>
-                <td className={s.row}><button>Edit</button><button onClick={() => dispatch(deleteTransactionThunk(transaction.id))}>Delete</button></td>
-                </tr>
-  )
+    <tr className={s.rowline}>
+      <td className={s.row}>{transaction.transactionDate}</td>
+      <td className={s.row}>{transaction.type === "INCOME" ? "+" : "-"}</td>
+      <td className={s.row}>{transaction.categoryId}</td>
+      <td className={s.row}>{transaction.comment}</td>
+      <td className={s.row}>{transaction.amount}</td>
+      <td className={s.row}>
+        <button onClick={openModal}>Edit</button>
+        {isOpen && (
+          <Modal title="Edit transaction" closeModal={closeModal}>
+            <EditTransactionForm
+              transaction={transaction}
+              closeModal={closeModal}
+            />
+          </Modal>
+        )}
+        <button
+          onClick={() => dispatch(deleteTransactionThunk(transaction.id))}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
 }
 
-export default TransactionItem
+export default TransactionItem;
