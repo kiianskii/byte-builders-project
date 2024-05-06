437145ddb49d3b65fca383283d11b253a1ee65eb
AlexHrab <151546761+AlexHrab@users.noreply.github.com>
2024-05-06 21:59:01 +0300

    up

---
+++ src/pages/StatisticsTab/constans.js
@@ -0,0 +1,43 @@
+export const categoryes = [
+  { categoryName: "Main expenses", categoryColor: "#FED057" },
+  { categoryName: "Products", categoryColor: "#FFD8D0" },
+  { categoryName: "Car", categoryColor: "#FD9498" },
+  { categoryName: "Self care", categoryColor: "#C5BAFF" },
+  { categoryName: "Child care", categoryColor: "#6E78E8" },
+  { categoryName: "Household products", categoryColor: "#4A56E2" },
+  { categoryName: "Education", categoryColor: "#81E1FF" },
+  { categoryName: "Leisure", categoryColor: "#24CCA7" },
+  { categoryName: "Other expenses", categoryColor: "#00AD84" },
+  { categoryName: "Entertainment", categoryColor: "#FF868D" },
+  { categoryName: "Income", categoryColor: "#FFB627" },
+];
+
+export const selectMonth = [
+  { value: "1", label: "January" },
+  { value: "2", label: "February" },
+  { value: "3", label: "March" },
+  { value: "4", label: "April" },
+  { value: "5", label: "May" },
+  { value: "6", label: "June" },
+  { value: "7", label: "July" },
+  { value: "8", label: "August" },
+  { value: "9", label: "September" },
+  { value: "10", label: "October" },
+  { value: "11", label: "November" },
+  { value: "12", label: "December" },
+];
+
+export const selectYear = [
+  { value: "2024", label: "2024" },
+  { value: "2023", label: "2023" },
+  { value: "2022", label: "2022" },
+  { value: "2021", label: "2021" },
+  { value: "2020", label: "2020" },
+  { value: "2019", label: "2019" },
+  { value: "2018", label: "2018" },
+  { value: "2017", label: "2017" },
+  { value: "2016", label: "2016" },
+  { value: "2015", label: "2015" },
+  { value: "2014", label: "2014" },
+  { value: "2013", label: "2013" },
+];
